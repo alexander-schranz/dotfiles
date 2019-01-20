a853f3cd40dddbd4adbe9c1f866f38c8051b2d21
Alexander Schranz <alexander@sulu.io>
2019-01-20 15:51:01 +0100

    Update fish prompt for better git integration

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,22 +1,92 @@
-set fish_git_dirty_color red
-set fish_git_not_dirty_color green
+function fish_prompt --description 'Write out the prompt'
+	if not set -q __fish_git_prompt_show_informative_status
+		set -g __fish_git_prompt_show_informative_status 1
+	end
+	if not set -q __fish_git_prompt_hide_untrackedfiles
+		set -g __fish_git_prompt_hide_untrackedfiles 1
+	end
 
-function parse_git_branch
-  set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
-  set -l git_status (git status -s)
+	if not set -q __fish_git_prompt_color_branch
+		set -g __fish_git_prompt_color_branch magenta --bold
+	end
+	if not set -q __fish_git_prompt_showupstream
+		set -g __fish_git_prompt_showupstream "informative"
+	end
+	if not set -q __fish_git_prompt_char_upstream_ahead
+		set -g __fish_git_prompt_char_upstream_ahead "↑"
+	end
+	if not set -q __fish_git_prompt_char_upstream_behind
+		set -g __fish_git_prompt_char_upstream_behind "↓"
+	end
+	if not set -q __fish_git_prompt_char_upstream_prefix
+		set -g __fish_git_prompt_char_upstream_prefix ""
+	end
 
-  if test -n "$git_status"
-    echo (set_color $fish_git_dirty_color)$branch(set_color normal)
-  else
-    echo (set_color $fish_git_not_dirty_color)$branch(set_color normal)
-  end
-end
+	if not set -q __fish_git_prompt_char_stagedstate
+		set -g __fish_git_prompt_char_stagedstate "●"
+	end
+	if not set -q __fish_git_prompt_char_dirtystate
+		set -g __fish_git_prompt_char_dirtystate "✚"
+	end
+	if not set -q __fish_git_prompt_char_untrackedfiles
+		set -g __fish_git_prompt_char_untrackedfiles "…"
+	end
+	if not set -q __fish_git_prompt_char_conflictedstate
+		set -g __fish_git_prompt_char_conflictedstate "✖"
+	end
+	if not set -q __fish_git_prompt_char_cleanstate
+		set -g __fish_git_prompt_char_cleanstate "✔"
+	end
 
-function fish_prompt
-  set -l git_dir (git rev-parse --git-dir 2> /dev/null)
-  if test -n "$git_dir"
-    printf '%s%s%s:%s> ' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (parse_git_branch)
-  else
-    printf '%s%s%s> ' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
-  end
+	if not set -q __fish_git_prompt_color_dirtystate
+		set -g __fish_git_prompt_color_dirtystate blue
+	end
+	if not set -q __fish_git_prompt_color_stagedstate
+		set -g __fish_git_prompt_color_stagedstate yellow
+	end
+	if not set -q __fish_git_prompt_color_invalidstate
+		set -g __fish_git_prompt_color_invalidstate red
+	end
+	if not set -q __fish_git_prompt_color_untrackedfiles
+		set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+	end
+	if not set -q __fish_git_prompt_color_cleanstate
+		set -g __fish_git_prompt_color_cleanstate green --bold
+	end
+
+	set -l last_status $status
+
+	if not set -q __fish_prompt_normal
+		set -g __fish_prompt_normal (set_color normal)
+	end
+
+	set -l color_cwd
+	set -l prefix
+	switch $USER
+	case root toor
+		if set -q fish_color_cwd_root
+			set color_cwd $fish_color_cwd_root
+		else
+			set color_cwd $fish_color_cwd
+		end
+		set suffix '#'
+	case '*'
+		set color_cwd $fish_color_cwd
+		set suffix '$'
+	end
+
+	# PWD
+	set_color $color_cwd
+	echo -n (prompt_pwd)
+	set_color normal
+
+	printf '%s ' (__fish_vcs_prompt)
+
+	if not test $last_status -eq 0
+	set_color $fish_color_error
+	end
+
+	echo -n "$suffix "
+
+	set_color normal
 end
